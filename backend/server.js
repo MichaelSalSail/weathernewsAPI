80e13a14e60bcfe24c00ddf50a8d5b2667325fb3
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-06-16 10:56:06 -0400

    Attempted OpenAI API implementation

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const logger = require('morgan'); // Adds Http logging into the console
+const { Configuration, OpenAIApi } = require("openai");
+const credentials=require('./API_keys.json');
+const configuration = new Configuration({
+    apiKey: credentials.openai.apiKey_Michael,
+});
+const openai = new OpenAIApi(configuration);
+
+async function submitForm() {
+    let options = {
+        model: 'text-davinci-003',
+        temperature: 0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ['/'],
+        prompt: "What is the diameter of earth?"
+    };
+    const chat_completion = await openai.createChatCompletion(options);
+    console.log(chat_completion);
+}
+// 'insufficient_quota'...
+// My free trial expired so my APIkay can't make requests
+// submitForm();
