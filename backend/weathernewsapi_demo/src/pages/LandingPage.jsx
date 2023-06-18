3350ca1fcd3777d74119f0ea3d79274256653249
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-06-18 12:30:54 -0400

    Display neatly organized JSONs in demo app page.

--- backend/weathernewsapi_demo/src/pages/LandingPage.jsx
+++ backend/weathernewsapi_demo/src/pages/LandingPage.jsx
@@ -3,7 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
     const navigate = useNavigate();
-    const [show, setShow] = useState('PLACEHOLDER');
+    const [current, setCurrent] = useState('CURRENT WEATHER DATA GOES HERE');
+    const [forecast, setForecast] = useState('7-DAY FORECAST WEATHER DATA GOES HERE');
+    const [show, setShow] = useState('NEWS ARTICLES GO HERE');
+    const [exist, setExist] = useState('???');
     const [text, setText] = useState('');
     const handleChange = (event) => {
         setText(event.target.value);
@@ -22,14 +25,21 @@ export default function LandingPage() {
             }).then(res => res.json())
             .then(data => {
                 console.log("Successful POST signatureRequest!",data);
-                setShow(JSON.stringify(data, null, 2));
+                setShow(JSON.stringify(data.news_response.data, null, 2));
+                setCurrent(JSON.stringify(data.weather_response.current, null, 2));
+                setForecast(JSON.stringify(data.weather_response.forecast, null, 2));
+                if(data.news_response.news_in_location===true)
+                    setExist("Yes");
+                else
+                    setExist("No");
             }).catch((error) => {
                 console.log("Unsuccessful POST signatureRequest.",error);
             });
     }
     return (
     <div>
-        <h1>
+        <h3>
+        <p>format: (latitude, longitude) &#123;location_name&#125;</p>
         <input
             type="text"
             value={text}
@@ -46,7 +56,10 @@ export default function LandingPage() {
         >
             Submit
         </button>
-        </h1>
+        </h3>
+        <p>{current}</p>
+        <p>{forecast}</p>
+        <p>DOES THE LOCATION EXIST? {exist}</p>
         <p>{show}</p>
     </div>
     );
