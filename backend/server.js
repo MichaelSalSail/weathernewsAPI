6de2ead31efdd5f65d3bb0c9fecfd768610ba1ae
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-06-17 22:35:38 -0400

    Added docstrings for all API call functions.

--- backend/server.js
+++ backend/server.js
@@ -12,24 +12,12 @@ const request = require('request');
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(credentials.news_api.apiKey);
 
-async function submitGPT() {
-    let options = {
-        model: 'text-davinci-003',
-        temperature: 0,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ['/'],
-        prompt: "What is the diameter of earth?"
-    };
-    const chat_completion = await openai.createChatCompletion(options);
-    console.log(chat_completion);
-}
-// 'insufficient_quota'...
-// My free trial expired so my APIkay can't make requests
-// submitGPT();
-
+/**
+ * Retrieves current and future weather data for the location through Open-Meteo API
+ * @param {float} latitude - latitude for location
+ * @param {float} longitude - longitude for location
+ * @return {json} - current weather and 7 day forecast
+ */
 function submitMeteo(latitude, longitude) {
     // API Doc: https://open-meteo.com/en/docs
     let weather_resp={weather_response:{current:{temperature:0,weathercode:0},forecast:{}}}
@@ -91,6 +79,11 @@ function submitMeteo(latitude, longitude) {
 // NY,NY coordinates
 submitMeteo(40.71,-74.01)
 
+/**
+ * Retrieves latest weather news for the location, if it exists, through NewsAPI
+ * @param {float} location_name - state, city, or town (ex. New Jersey)
+ * @return {json} - metadata (title, url, date) on weather news articles (max. 10)
+ */
 async function submitNews(location_name) {
     let news_resp={news_response:{news_in_location:true,data:[]}}
     let result=await newsapi.v2.everything({
@@ -131,3 +124,22 @@ async function submitNews(location_name) {
     }
 }
 submitNews("New York")
+
+// sample code to ask ChatGPT a question
+async function submitGPT() {
+    let options = {
+        model: 'text-davinci-003',
+        temperature: 0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ['/'],
+        prompt: "What is the diameter of earth?"
+    };
+    const chat_completion = await openai.createChatCompletion(options);
+    console.log(chat_completion);
+}
+// 'insufficient_quota'...
+// My free trial expired so my APIkay can't make requests
+// submitGPT();
